228b396b2911ff4b7a10215fd32a13695982962f
Jon Winsley <jon.winsley@gmail.com>
2021-06-24 03:22:32 -0400

    Adding missing exports

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ export * from "./widgets/Bar";
 export * from "./widgets/Grid";
 export * from "./widgets/Rectangle";
 export * from "./widgets/Table";
+export * from "./widgets/Label";
+export * from "./widgets/LineChart/LineChart";
 
 export { loop } from "./demo";
