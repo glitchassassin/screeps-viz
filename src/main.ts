6e94000a04da5a483f71488981aaee3fbb0f9bea
Jon Winsley <jon.winsley@gmail.com>
2022-07-12 16:38:20 -0400

    Fixing export

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ export * from "./widgets/Rectangle";
 export * from "./widgets/Table";
 export * from "./widgets/Label";
 export * from "./widgets/LineChart/LineChart";
+export * from "./widgets/Dial";
 export * as Metrics from "./metrics/Timeseries";
 
 export { loop } from "./demo";
